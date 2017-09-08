a13c75b7fed2e226b3fd2b8121257e79efed8313
ricky han <rickylqhan@gmail.com>
2017-09-07 22:55:44 -0400

    fixed untracked files

--- dist/utils.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function toPair(market) {
-    return market.replace("-", "_").toLowerCase();
-}
-exports.toPair = toPair;
-//# sourceMappingURL=utils.js.map
